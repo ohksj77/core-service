d9448a4bdc3f6dc61cc585c2b3e5aef7e0e70067
김승진 <ohksj77@gmail.com>
2024-11-02 22:36:08 +0900

    feat: add argument resolver and auth annotation

---
+++ src/main/kotlin/com/dragonguard/core/global/auth/AuthorizedMemberIdArgumentResolver.kt
@@ -0,0 +1,28 @@
+package com.dragonguard.core.global.auth
+
+import com.dragonguard.core.config.security.oauth.user.UserPrinciple
+import org.springframework.core.MethodParameter
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Component
+class AuthorizedMemberIdArgumentResolver : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean =
+        parameter.hasParameterAnnotation(AuthorizedMemberId::class.java) &&
+            parameter.getParameterType() == Long::class.java
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): Any? {
+        val principal: UserPrinciple =
+            SecurityContextHolder.getContext().authentication.principal as UserPrinciple
+        return principal.getId()
+    }
+}
