f2e2934cbb9137a10a62b2c9ac873384acf2988c
김승진 <ohksj77@gmail.com>
2024-12-24 12:58:14 +0900

    docs: update api docs

--- src/test/kotlin/com/dragonguard/core/domain/gitrepo/GitRepoControllerTest.kt
+++ src/test/kotlin/com/dragonguard/core/domain/gitrepo/GitRepoControllerTest.kt
@@ -1,5 +1,6 @@
 package com.dragonguard.core.domain.gitrepo
 
+import com.dragonguard.core.domain.member.Member
 import com.dragonguard.core.support.docs.RestDocsTest
 import com.dragonguard.core.support.docs.RestDocsUtils
 import com.dragonguard.core.support.fixture.GitRepoFactory
@@ -14,6 +15,8 @@ import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
+import org.springframework.restdocs.request.RequestDocumentation.queryParameters
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -62,6 +65,169 @@ class GitRepoControllerTest : RestDocsTest() {
                         fieldWithPath("code").description("http 상태 코드"),
                         fieldWithPath("message").description("성공 메시지"),
                         fieldWithPath("data[]").description("git-org 이름"),
+                    )
+                ),
+            )
+    }
+
+    @Test
+    fun `git-orgs git-repos 조회`() {
+        // given
+        val expected = GitRepoFactory.createGitOrgGitRepoResponse()
+
+        BDDMockito
+            .given(
+                gitRepoService.getGitOrgGitRepos(any(String::class.java), any(Member::class.java)),
+            ).willReturn(expected)
+
+        // when
+        val perform: ResultActions =
+            mockMvc.perform(
+                RestDocumentationRequestBuilders
+                    .get("/git-repos/git-organizations")
+                    .queryParam("name", "tukcom2023CD")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer apfawfawfa.awfsfawef2.r4svfv32"),
+            )
+
+        // then
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+
+        // docs
+        perform
+            .andDo(MockMvcResultHandlers.print())
+            .andDo(
+                document(
+                    "get git-orgs git-repos",
+                    RestDocsUtils.getDocumentRequest(),
+                    RestDocsUtils.getDocumentResponse(),
+                    requestHeaders(
+                        headerWithName("Authorization").description("Bearer jwt 액세스 토큰"),
+                    ),
+                    queryParameters(
+                        parameterWithName("name").description("git-org 이름"),
+                    ),
+                    responseFields(
+                        fieldWithPath("code").description("http 상태 코드"),
+                        fieldWithPath("message").description("성공 메시지"),
+                        fieldWithPath("data.profile_image").description("git-repo 프로필 이미지"),
+                        fieldWithPath("data.git_repos[]").description("git-repo 이름"),
+                    )
+                ),
+            )
+    }
+
+    @Test
+    fun `git-repos 기여도 상세 조회`() {
+        // given
+        val expected = GitRepoFactory.createGitRepoResponse()
+
+        BDDMockito
+            .given(
+                gitRepoService.getGitRepoDetails(any(String::class.java), any(Long::class.java)),
+            ).willReturn(expected)
+
+        // when
+        val perform: ResultActions =
+            mockMvc.perform(
+                RestDocumentationRequestBuilders
+                    .get("/git-repos")
+                    .queryParam("name", "tukcom2023CD/DragonGuard-JinJin")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer apfawfawfa.awfsfawef2.r4svfv32"),
+            )
+
+        // then
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+
+        // docs
+        perform
+            .andDo(MockMvcResultHandlers.print())
+            .andDo(
+                document(
+                    "get git-repos details",
+                    RestDocsUtils.getDocumentRequest(),
+                    RestDocsUtils.getDocumentResponse(),
+                    requestHeaders(
+                        headerWithName("Authorization").description("Bearer jwt 액세스 토큰"),
+                    ),
+                    queryParameters(
+                        parameterWithName("name").description("git-repo 이름"),
+                    ),
+                    responseFields(
+                        fieldWithPath("code").description("http 상태 코드"),
+                        fieldWithPath("message").description("성공 메시지"),
+                        fieldWithPath("data.spark_line[]").description("git-repo 기여도 추이"),
+                        fieldWithPath("data.git_repo_members[].github_id").description("git-repo 멤버 githubId"),
+                        fieldWithPath("data.git_repo_members[].profile_url").description("git-repo 멤버 프로필 이미지"),
+                        fieldWithPath("data.git_repo_members[].commits").description("git-repo 멤버 커밋 수"),
+                        fieldWithPath("data.git_repo_members[].additions").description("git-repo 멤버 코드 추가 수"),
+                        fieldWithPath("data.git_repo_members[].deletions").description("git-repo 멤버 코드 삭제 수"),
+                        fieldWithPath("data.git_repo_members[].is_service_member").description("git-repo 멤버 서비스 멤버 여부"),
+                    )
+                ),
+            )
+    }
+
+    @Test
+    fun `git-repos 기여도 상세 비교`() {
+        // given
+        val expected = GitRepoFactory.createGitRepoCompareResponse()
+
+        BDDMockito
+            .given(
+                gitRepoService.compare(any(String::class.java), any(String::class.java), any(Long::class.java)),
+            ).willReturn(expected)
+
+        // when
+        val perform: ResultActions =
+            mockMvc.perform(
+                RestDocumentationRequestBuilders
+                    .get("/git-repos/compare")
+                    .queryParam("first", "tukcom2023CD/DragonGuard-JinJin")
+                    .queryParam("second", "GitRank-v2/core-service")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer apfawfawfa.awfsfawef2.r4svfv32"),
+            )
+
+        // then
+        perform
+            .andExpect(MockMvcResultMatchers.status().isOk())
+
+        // docs
+        perform
+            .andDo(MockMvcResultHandlers.print())
+            .andDo(
+                document(
+                    "get git-repos compare",
+                    RestDocsUtils.getDocumentRequest(),
+                    RestDocsUtils.getDocumentResponse(),
+                    requestHeaders(
+                        headerWithName("Authorization").description("Bearer jwt 액세스 토큰"),
+                    ),
+                    queryParameters(
+                        parameterWithName("first").description("첫번째 git-repo 이름"),
+                        parameterWithName("second").description("두번째 git-repo 이름"),
+                    ),
+                    responseFields(
+                        fieldWithPath("code").description("http 상태 코드"),
+                        fieldWithPath("message").description("성공 메시지"),
+                        fieldWithPath("data.first.spark_line[]").description("git-repo 기여도 추이"),
+                        fieldWithPath("data.first.git_repo_members[].github_id").description("git-repo 멤버 githubId"),
+                        fieldWithPath("data.first.git_repo_members[].profile_url").description("git-repo 멤버 프로필 이미지"),
+                        fieldWithPath("data.first.git_repo_members[].commits").description("git-repo 멤버 커밋 수"),
+                        fieldWithPath("data.first.git_repo_members[].additions").description("git-repo 멤버 코드 추가 수"),
+                        fieldWithPath("data.first.git_repo_members[].deletions").description("git-repo 멤버 코드 삭제 수"),
+                        fieldWithPath("data.first.git_repo_members[].is_service_member").description("git-repo 멤버 서비스 멤버 여부"),
+                        fieldWithPath("data.second.spark_line[]").description("git-repo 기여도 추이"),
+                        fieldWithPath("data.second.git_repo_members[].github_id").description("git-repo 멤버 githubId"),
+                        fieldWithPath("data.second.git_repo_members[].profile_url").description("git-repo 멤버 프로필 이미지"),
+                        fieldWithPath("data.second.git_repo_members[].commits").description("git-repo 멤버 커밋 수"),
+                        fieldWithPath("data.second.git_repo_members[].additions").description("git-repo 멤버 코드 추가 수"),
+                        fieldWithPath("data.second.git_repo_members[].deletions").description("git-repo 멤버 코드 삭제 수"),
+                        fieldWithPath("data.second.git_repo_members[].is_service_member").description("git-repo 멤버 서비스 멤버 여부"),
                     )
                 ),
             )
--- src/test/kotlin/com/dragonguard/core/support/fixture/GitRepoFactory.kt
+++ src/test/kotlin/com/dragonguard/core/support/fixture/GitRepoFactory.kt
@@ -1,5 +1,10 @@
 package com.dragonguard.core.support.fixture
 
+import com.dragonguard.core.domain.gitrepo.dto.GitOrgGitRepoResponse
+import com.dragonguard.core.domain.gitrepo.dto.GitRepoCompareResponse
+import com.dragonguard.core.domain.gitrepo.dto.GitRepoMemberResponse
+import com.dragonguard.core.domain.gitrepo.dto.GitRepoResponse
+
 class GitRepoFactory {
 
     companion object {
@@ -8,6 +13,37 @@ class GitRepoFactory {
             "test-git-repo1",
             "test-git-repo2",
             "test-git-repo3",
+        )
+
+        fun createGitOrgGitRepoResponse() = GitOrgGitRepoResponse(
+            "test-profile-image",
+            setOf("test-git-repo1", "test-git-repo2", "test-git-repo3")
+        )
+
+        fun createGitRepoResponse() = GitRepoResponse(
+            listOf(1, 2, 3, 4, 5),
+            listOf(
+                GitRepoMemberResponse(
+                    "test-member1",
+                    "test-member1-profile-image",
+                    1000,
+                    1000,
+                    1000,
+                    false,
+                ),
+                GitRepoMemberResponse(
+                    "test-member2",
+                    "test-member2-profile-image",
+                    2000,
+                    3000,
+                    4000,
+                    true,
+                ),
+            ),
+        )
+
+        fun createGitRepoCompareResponse() = GitRepoCompareResponse(
+            createGitRepoResponse(), createGitRepoResponse(),
         )
     }
 }
